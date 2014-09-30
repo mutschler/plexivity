66509da3f43e724eef29f69ef3aac82e15d52f07
Raphael Mutschler <info@raphaelmutschler.de>
2014-10-01 01:50:59 +0200

    reformatting

--- app/helper.py
+++ app/helper.py
@@ -3,48 +3,51 @@
 
 from app import db, models
 
+
 def statistics():
-	pass
+    pass
+
 
 def getPercentage(viewed, duration):
-	percent = "%2d" % ( ( float(viewed) / float(duration) ) * 100 )
+    percent = "%2d" % ((float(viewed) / float(duration)) * 100)
 
-	if int(percent) >= 90:
-		return 100
-	return percent
+    if int(percent) >= 90:
+        return 100
+    return percent
+
 
 def playerImage(platform):
-	if platform == "Roku":
-		return "images/platforms/roku.png"
-	elif platform == "Apple TV":
-		return "images/platforms/appletv.png"
-	elif platform == "Firefox":
-		return "images/platforms/firefox.png"
-	elif platform == "Chromecast":
-		return "images/platforms/chromecast.png"
-	elif platform == "Chrome":
-		return "images/platforms/chrome.png"
-	elif platform == "Android":
-		return "images/platforms/android.png"
-	elif platform == "Nexus":
-		return "images/platforms/android.png"
-	elif platform == "iPad":
-		return "images/platforms/ios.png"
-	elif platform == "iPhone":
-		return "images/platforms/ios.png"
-	elif platform == "iOS":
-		return "images/platforms/ios.png"
-	elif platform == "Plex Home Theater":
-		return "images/platforms/pht.png"
-	elif platform == "Linux/RPi-XBMC":
-		return "images/platforms/xbmc.png"
-	elif platform == "Safari":
-		return "images/platforms/safari.png"
-	elif platform == "Internet Explorer":
-		return "images/platforms/ie.png"
-	elif platform == "Unknown Browser":
-		return "images/platforms/default.png"
-	elif platform == "Windows-XBMC":
-		return "images/platforms/xbmc.png"
-	else:
-		return "images/platforms/default.png"
+    if platform == "Roku":
+        return "images/platforms/roku.png"
+    elif platform == "Apple TV":
+        return "images/platforms/appletv.png"
+    elif platform == "Firefox":
+        return "images/platforms/firefox.png"
+    elif platform == "Chromecast":
+        return "images/platforms/chromecast.png"
+    elif platform == "Chrome":
+        return "images/platforms/chrome.png"
+    elif platform == "Android":
+        return "images/platforms/android.png"
+    elif platform == "Nexus":
+        return "images/platforms/android.png"
+    elif platform == "iPad":
+        return "images/platforms/ios.png"
+    elif platform == "iPhone":
+        return "images/platforms/ios.png"
+    elif platform == "iOS":
+        return "images/platforms/ios.png"
+    elif platform == "Plex Home Theater":
+        return "images/platforms/pht.png"
+    elif platform == "Linux/RPi-XBMC":
+        return "images/platforms/xbmc.png"
+    elif platform == "Safari":
+        return "images/platforms/safari.png"
+    elif platform == "Internet Explorer":
+        return "images/platforms/ie.png"
+    elif platform == "Unknown Browser":
+        return "images/platforms/default.png"
+    elif platform == "Windows-XBMC":
+        return "images/platforms/xbmc.png"
+    else:
+        return "images/platforms/default.png"

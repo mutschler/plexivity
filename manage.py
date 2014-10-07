7f09d5199d38f4251c0eaff0061132b301b3356c
Raphael Mutschler <info@raphaelmutschler.de>
2014-10-07 02:01:24 +0200

    optinal ssl connections

--- manage.py
+++ manage.py
@@ -8,7 +8,18 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, models.db)
 manager = Manager(app)
 
+@manager.command
+def ssl():
+    from OpenSSL import SSL
+    ctx = SSL.Context(SSL.SSLv23_METHOD)
+    ctx.use_privatekey_file('ssl.key')
+    ctx.use_certificate_file('ssl.cert')
+    app.run(ssl_context=ctx)
+
 manager.add_command('db', MigrateCommand)
+
+
+
 
 if __name__ == "__main__":
     manager.run()

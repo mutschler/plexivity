ff4a4b3ef8b073a83e24e0befc42ba4a68ccc148
Raphael Mutschler <info@raphaelmutschler.de>
2014-12-04 15:52:05 +0100

    chg: add admin view to delete History entries and cached files

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ import os
 
 from app import config, plex
 
-from flask import Flask, g, request
+from flask import Flask, g, request, redirect, url_for
 from flask.ext.sqlalchemy import SQLAlchemy
 
 from flask.ext.babel import Babel
@@ -36,3 +36,53 @@ lm.init_app(app)
 lm.login_view = 'login'
 
 from app import views, models
+
+## try import admin view and functions if module is not there, just skip this for now
+try:
+    from flask.ext.admin import Admin, AdminIndexView
+    from flask.ext.admin.contrib.sqla import ModelView
+    from flask.ext.admin.contrib import fileadmin
+    from flask.ext.admin import expose
+
+    # Create customized model view class
+    class MyModelView(ModelView):
+
+        def is_accessible(self):
+            return current_user.is_authenticated()
+
+
+    # Create customized index view class that handles login
+    class MyAdminIndexView(AdminIndexView):
+
+        @expose('/')
+        def index(self):
+            if not current_user.is_authenticated():
+                return redirect(url_for('login'))
+            return super(MyAdminIndexView, self).index()
+
+    class UserView(ModelView):
+        can_create = False
+
+        # Override displayed fields
+        column_list = ('email', 'locale')
+
+        def __init__(self, session, **kwargs):
+            # You can pass name and other parameters if you want to
+            super(UserView, self).__init__(models.User, db.session, **kwargs)
+
+
+    class HistoryView(ModelView):
+        can_create = False
+        column_list = ('time', 'user', 'title', 'platform', 'notified', 'stopped', 'paused', 'duration', 'view_offset')
+
+        def __init__(self, session, **kwargs):
+            # You can pass name and other parameters if you want to
+            super(HistoryView, self).__init__(models.Processed, db.session, **kwargs)
+
+    admin = Admin(app, name="plexivity", index_view=MyAdminIndexView())
+
+    admin.add_view(UserView(db.session))
+    admin.add_view(HistoryView(db.session, name="History"))
+    admin.add_view(fileadmin.FileAdmin(config.DATA_DIR + '/cache/', name='Cached Files'))
+except:
+    pass

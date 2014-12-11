842fe296c031e27b471336e07d270cd33db5a4bd
Raphael Mutschler <info@raphaelmutschler.de>
2014-12-11 15:07:25 +0100

    chg: changed file size of log file to 2MB @minor

--- app/logger.py
+++ app/logger.py
@@ -11,7 +11,7 @@ DEBUG = logging.DEBUG
 
 logger = logging.getLogger('plexivity')
 formatter = logging.Formatter('%(asctime)s %(name)s\t %(levelname)-8s: %(message)s', '%d.%m.%Y %H:%M:%S')
-rotation = logging.handlers.RotatingFileHandler(os.path.join(config.DATA_DIR, "plexivity.log"), maxBytes=5 * 1024 * 1024, backupCount=5)
+rotation = logging.handlers.RotatingFileHandler(os.path.join(config.DATA_DIR, "plexivity.log"), maxBytes=2 * 1024 * 1024, backupCount=5)
 rotation.setFormatter(formatter)
 logger.addHandler(rotation)
 
